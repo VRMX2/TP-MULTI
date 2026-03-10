13b420d1089397f05013d06c03cda14fa3ed0008
VRMX2 <lehcengrissi@gmail.com>
2026-03-10 04:08:25 -0700

    new commit

--- TP4/main.py
+++ TP4/main.py
@@ -1,64 +1,40 @@
 import cv2
 import numpy as np
 
-
 image = cv2.imread("image.bmp", cv2.IMREAD_GRAYSCALE)
-_, binary = cv2.threshold(image, 127, 1, cv2.THRESH_BINARY)
+_, image_bin = cv2.threshold(image,127,1,cv2.THRESH_BINARY)
 
-print("Binary image:")
-print(binary)
+fimage = image_bin.flatten()
 
-
-fimage = binary.flatten()
-
-print("\nFlattened image:")
-print(fimage)
-
-
-rle = []
-count = 1
-prev = fimage[0]
+rle=[]
+count=1
+prev=fimage[0]
 
 for pixel in fimage[1:]:
-    if pixel == prev:
-        count += 1
+    if pixel==prev:
+        count+=1
     else:
-        rle.append((count, prev))
-        prev = pixel
-        count = 1
+        rle.append((count,prev))
+        count=1
+        prev=pixel
 
-rle.append((count, prev))
+rle.append((count,prev))
 
-print("\nRLE tuples:")
-print(rle)
+code=""
 
-encoded = ""
+for count,value in rle:
+    count_bits="{:03}".format(count)
+    code+=count_bits+str(value)
 
-for count, value in rle:
-    count_str = "{:03}".format(count)
-    encoded += count_str + str(value)
+with open("compression.txt","w") as file:
+    file.write(code)
 
-print("\nEncoded string:")
-print(encoded)
+taille_code=len(code)
+taille_image=len(fimage)
 
-file_name = "compressed.txt"
+taux=(taille_code/taille_image)*100
 
-with open(file_name, "w") as file:
-    file.write(encoded)
-
-print("\nCompressed data written to file.")
-
-with open(file_name, "r") as file:
-    txt = file.readlines()[0]
-
-print("\nData read from file:")
-print(txt)
-
-original_bits = binary.size
-encoded_bits = len(encoded)
-
-compression_rate = (encoded_bits / original_bits) * 100
-
-print("\nOriginal size (bits):", original_bits)
-print("Encoded size (bits):", encoded_bits)
-print("Compression rate:", compression_rate, "%")
+print("Code :",code)
+print("Taille code :",taille_code)
+print("Taille image :",taille_image)
+print("Taux de compression :",taux,"%")
